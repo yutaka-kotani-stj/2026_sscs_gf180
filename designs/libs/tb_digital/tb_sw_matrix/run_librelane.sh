349da0fab1751dbfee3fa29ad7d3960fd6b2e91a
Yutaka KOTANI <yutaka.kotani.in@gmail.com>
2026-08-13 06:58:16 +0200

    add sw matrix

---
+++ designs/libs/tb_digital/tb_sw_matrix/run_librelane.sh
@@ -0,0 +1,2 @@
+sak-pdk-script.sh gf180mcuD
+librelane --pdk gf180mcuD --pdk-root /foss/pdks --manual-pdk --run-tag demo config.yaml
---
+++ designs/libs/tb_digital/tb_sw_matrix/trans_gate.v
@@ -0,0 +1,16 @@
+module trans_gate (
+    input  wire in,
+    input  wire en,
+    output wire out
+);
+
+    // Explicitly instantiate the GF180 5V Tristate Buffer
+    // This acts exactly like a transmission gate switch in digital circuits
+    // OE = Output Enable (Active High)
+    gf180mcu_fd_sc_mcu7t5v0__bufz_8 structural_tg (
+        .I(in),
+        .EN(en),
+        .Z(out)
+    );
+
+endmodule
